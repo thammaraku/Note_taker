b502b8f31347a36f9d8376edf9fc56063170912f
thammaraku <thammaraku@gmail.com>
2021-01-03 00:15:41 -0600

    remove function updatedb

--- routes/routes.js
+++ routes/routes.js
@@ -80,15 +80,6 @@ module.exports = app => {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
-
-        //updates the json file whenever a note is added or deleted
-        function updateDb() {
-            fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-                if (err) throw err;
-                return true;
-            });
-        }
-
     });
 
 
